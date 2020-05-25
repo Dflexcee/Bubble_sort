81b9709a2030f138559cff74bbbe76c265805b82
dflexcee <dflexcee@gmail.com>
2020-05-25 07:16:11 -0700

    i am wroking on my robucop our to collect errors

---
+++ bubble_sort.rb
@@ -0,0 +1,32 @@
+def bubble_sort(arr)
+  idx = 0
+    while idx < arr.length do
+      arr.each do |x|
+        a = arr.index(x)
+          y = arr.index(x)+1
+            if arr[y] != nil
+              if x > arr[y]
+                arr[a], arr[y] = arr[y], arr[a]
+              end
+        end
+      end
+      idx += 1
+    end
+  arr
+end
+
+def bubble_sort_by(arr)
+  loop do
+    flag = false
+ arr.each_with_index do |_val, i|
+    next if i == arr.length - 1
+
+    if yield(arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      flag = true
+    end
+  end
+    break unless flag
+  end
+  arr
+end
